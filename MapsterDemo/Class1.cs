7e431385c07b813ca7935fb9202b590ca5085eef
Allen <1756196683@qq.com>
2022-04-03 23:59:19 +0800

    添加项目文件。

---
+++ MapsterDemo/Class1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapsterDemo
+{
+    /// <summary>
+    /// 学生实体
+    /// </summary>
+    public class StudentEntity : BaseEntity
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string? Code { get; set; }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int? Age { get; set; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int? Sex { get; set; }
+
+        /// <summary>
+        /// 学校Id
+        /// </summary>
+        public int? SchoolId { get; set; }
+
+        //public SchoolEntity SchoolEntity { get; set; }
+
+        public IEnumerable<SchoolEntity> Entities { get; set; }
+    }
+
+    public class StudentEntityDTO : BaseEntity
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// 学号
+        /// </summary>
+        public string? Code { get; set; }
+
+        /// <summary>
+        /// 年龄
+        /// </summary>
+        public int? Age { get; set; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        public int? Sex { get; set; }
+
+        /// <summary>
+        /// 学校Id
+        /// </summary>
+        public int? SchoolId { get; set; }
+
+        //public SchoolEntity SchoolEntity { get; set; }
+
+        public IEnumerable<SchoolEntity> Entities { get; set; }
+    }
+
+    /// <summary>
+    /// 学校
+    /// </summary>
+    public class SchoolEntity : BaseEntity
+    {
+        /// <summary>
+        /// 学校名称
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// 学校代码
+        /// </summary>
+        public int? Code { get; set; }
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string? Adress { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public SchoolCategoryEnum? Category { get; set; }
+
+        public IEnumerable<StudentEntity> Entities { get; set; }
+    }
+
+    public class SchoolEntityDTO : BaseEntity
+    {
+        /// <summary>
+        /// 学校名称
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// 学校代码
+        /// </summary>
+        public int? Code { get; set; }
+
+        /// <summary>
+        /// 地址
+        /// </summary>
+        public string? Adress { get; set; }
+
+        /// <summary>
+        /// 类别
+        /// </summary>
+        public SchoolCategoryEnum? Category { get; set; }
+
+        public IEnumerable<StudentEntity> Entities { get; set; }
+    }
+    public class BaseEntity
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime? CreateDate { get; set; }
+
+        /// <summary>
+        /// 创建人Id
+        /// </summary>
+        public string? CreateUserId { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? UpdateDate { get; set; }
+
+        /// <summary>
+        /// 修改人Id
+        /// </summary>
+        public string? UpdateUserId { get; set; }
+    }
+
+    /// <summary>
+    /// 学校类别
+    /// </summary>
+    public enum SchoolCategoryEnum
+    {
+        PrimarySchool = 0,
+        HighSchool = 1,
+        University = 3,
+    }
+}
---
+++ MapsterDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Mapster;
+using MapsterDemo;
+
+Console.WriteLine("Hello, World!");
+var student = new StudentEntity
+{
+    Entities = new List<SchoolEntity> {
+        new SchoolEntity
+        {
+            Name="学生1"
+        }
+    }
+};
+var a = student.Adapt<StudentEntity, StudentEntityDTO>();
+
+var error = a.Entities;
+
+Console.ReadLine();
